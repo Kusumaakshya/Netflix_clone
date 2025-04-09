e7320e56f3d9061db8d31cb116603bd8e73eb5c5
Kusumaakshya <142290701+Kusumaakshya@users.noreply.github.com>
2025-04-09 17:00:36 +0530

    little changes here and there

--- netflix-backend/server.js
+++ netflix-backend/server.js
@@ -1,4 +1,5 @@
 // server.js
+// server.js
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -35,4 +36,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
